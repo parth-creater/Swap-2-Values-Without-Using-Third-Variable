8cce0beafc7a92118d2a8879a7846cc6f00d496d
parth-creater <79099863+parth-creater@users.noreply.github.com>
2021-02-16 00:10:30 +0530

    Add files via upload

---
+++ SWP.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("Enter First Value: ");
+scanf("%d",&a);
+printf("Enter Second Value: ");
+scanf("%d",&b);
+
+printf("\n After Swapping First Value: %d", b);
+printf("\n After Swapping Second Value: %d", a);
+getch();
+return 0;
+
+}
